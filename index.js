938297f937a71dccf7affbbafbe6e10e73d076cb
Beau Troxclair <beau.troxclair@gmail.com>
2020-03-04 16:21:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ function logWhisper(string){
 
 function sayHiToGrandma(string){
   if(string === string.toLowerCase){
-    return 'I can;t hear you!'
+    return "I can't hear you!"
   }
   else if(string === shout(string)){
     return "YES INDEED!"
   }
-  else if (sring === 'I love you, Grandma'){
+  else if (string === 'I love you, Grandma'){
     return "I love you, too."
   }
   
